6ef3a54388ec368a3e4297ba586b18032d1b0e7a
ramalhom <mario@ramalho.ch>
2017-09-22 22:50:14 +0200

    ok résultat

--- website/Controllers/TestController.cs
+++ website/Controllers/TestController.cs
@@ -37,7 +37,9 @@ namespace website.Controllers
 			var search = listRequest.Execute();
 			
             test.FirstSite = search.Items[0].DisplayLink;
+            //test.FirstSite = "infoteam.ch";
             test.TotalResults = search.SearchInformation.FormattedTotalResults;
+            //test.TotalResults = "850000";
             bool isFound = false;
             //int paging = 1;
             test.Sites = new string[10];
@@ -48,9 +50,12 @@ namespace website.Controllers
                 for (int i = 0; i < 10; i++)
                 {
                     test.Sites[i] = search.Items[i].DisplayLink;
+                    //test.Sites[i] = "Site numero " + i;
                     test.Urls[i] = "http://" + search.Items[i].FormattedUrl;
+                    //test.Urls[i] = "http://www.google.com";
                     test.Titles[i] = search.Items[i].Title;
-                    if (search.Items[i].FormattedUrl.Contains(test.WebSite))
+                    //test.Titles[i] = "Titre du site " + i;
+                    if (!isFound && search.Items[i].FormattedUrl.Contains(test.WebSite))
                     {
 
                         test.Position = i + 1;
